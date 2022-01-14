f5447bb236ed0f6d89cae61edf9e4614f24e4ed6
lanozubem <zubemluano@gmail.com>
2022-01-13 23:28:05 -0300

    Store-devtools adicionado!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { DisplayContadorComponent } from './display-contador/display-contador.component';
 import { StoreModule } from '@ngrx/store';
 import { appReducer } from './store/app.state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { appReducer } from './store/app.state';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ app: appReducer })
+    StoreModule.forRoot({ app: appReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
